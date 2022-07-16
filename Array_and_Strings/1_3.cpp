4c0a9837de5b217adad7e971cf6189fa600fae01
khyatig95 <85378102+khyatig95@users.noreply.github.com>
2022-07-16 11:04:27 -0700

    Update 1_3.cpp

--- Array_and_Strings/1_3.cpp
+++ Array_and_Strings/1_3.cpp
@@ -1,116 +1,56 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
-{\fonttbl\f0\fnil\fcharset0 Menlo-Bold;\f1\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red127\green0\blue85;\red42\green0\blue255;\red63\green127\blue95;
-\red0\green80\blue50;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720
+#include <iostream> 
+#include <string>  
+#include <cctype>
+using namespace std; 
+//Algorithm :   URLify : Iterate from the rear to shift non-white space 
+//              characters to the end and then add the %20 whenever space 
+//              is encountered
+//              Time : O(N) ; Space : O(1) as not using extra data structure
 
-\f0\b\fs24 \cf2 #include
-\f1\b0 \cf0  \cf3 <iostream>\cf0  \
+class myClass {
+    public : 
+            string URLify(string str);
+};
 
-\f0\b \cf2 #include
-\f1\b0 \cf0  \cf3 <string>\cf0   \
+string myClass::URLify(string str) {
+    bool true_length = false;
+    string::iterator tail = str.end()-1; //The end() points to a location after the end. That's why -1 is needed
+    for (auto itr=str.rbegin(); itr!=str.rend(); ++itr) {
+        cout << "Processing char " << *itr ;
+        if (isspace(*itr)) {
+            cout << " which is a white space" << endl;
+            if (!true_length)
+                continue;
+            else {
+                *tail = '0';
+                --tail;
+                *tail = '2';
+                --tail;
+                *tail = '%';
+                --tail;
+            }
+        } else {
+            cout << " which is NOT a whitespace" << endl;
+            if (!true_length) {
+                cout << "Set true_length to true " << endl;
+                true_length = true;
+            }
+            *tail = *itr;
+            --tail;
+            *itr = ' ';         //This is needed to delete the char from original position
+        }
+        cout << "String is " << str << endl;
+    }
+    return str;
+}
 
-\f0\b \cf2 #include
-\f1\b0 \cf0  \cf3 <cctype>\cf0 \
+int main() 
+{ 
+    myClass obj;
+    string test1 = " Khyati is coding         ";
 
-\f0\b \cf2 using
-\f1\b0 \cf0  
-\f0\b \cf2 namespace
-\f1\b0 \cf0  std; \
-\pard\pardeftab720
-\cf4 //Algorithm :   URLify : Iterate from the rear to shift non-white space \cf0 \
-\cf4 //              characters to the end and then add the %20 whenever space \cf0 \
-\cf4 //              is encountered\cf0 \
-\cf4 //              Time : O(N) ; Space : O(1) as not using extra data structure\cf0 \
-\
-\pard\pardeftab720
-
-\f0\b \cf2 class
-\f1\b0 \cf0  \cf5 myClass\cf0  \{\
+    string test2 = obj.URLify(test1);
+    cout << "String1 is " << test1 << " and its URLified version is " << test2 << endl;
     
-\f0\b \cf2 public
-\f1\b0 \cf0  : \
-            \cf5 string\cf0  
-\f0\b URLify
-\f1\b0 (\cf5 string\cf0  str);\
-\};\
-\
-\pard\pardeftab720
-\cf5 string\cf0  
-\f0\b myClass::URLify
-\f1\b0 (\cf5 string\cf0  str) \{\
-    
-\f0\b \cf2 bool
-\f1\b0 \cf0  true_length = 
-\f0\b \cf2 false
-\f1\b0 \cf0 ;\
-    \cf5 string\cf0 ::\cf5 iterator\cf0  tail = str.end()-1; \cf4 //The end() points to a location after the end. That's why -1 is needed\cf0 \
-    
-\f0\b \cf2 for
-\f1\b0 \cf0  (
-\f0\b \cf2 auto
-\f1\b0 \cf0  itr=str.rbegin(); itr!=str.rend(); ++itr) \{\
-        cout << \cf3 "Processing char "\cf0  << *itr ;\
-        
-\f0\b \cf2 if
-\f1\b0 \cf0  (isspace(*itr)) \{\
-            cout << \cf3 " which is a white space"\cf0  << endl;\
-            
-\f0\b \cf2 if
-\f1\b0 \cf0  (!true_length)\
-                
-\f0\b \cf2 continue
-\f1\b0 \cf0 ;\
-            
-\f0\b \cf2 else
-\f1\b0 \cf0  \{\
-                *tail = \cf3 '0'\cf0 ;\
-                --tail;\
-                *tail = \cf3 '2'\cf0 ;\
-                --tail;\
-                *tail = \cf3 '%'\cf0 ;\
-                --tail;\
-            \}\
-        \} 
-\f0\b \cf2 else
-\f1\b0 \cf0  \{\
-            cout << \cf3 " which is NOT a whitespace"\cf0  << endl;\
-            
-\f0\b \cf2 if
-\f1\b0 \cf0  (!true_length) \{\
-                cout << \cf3 "Set true_length to true "\cf0  << endl;\
-                true_length = 
-\f0\b \cf2 true
-\f1\b0 \cf0 ;\
-            \}\
-            *tail = *itr;\
-            --tail;\
-            *itr = \cf3 ' '\cf0 ;         \cf4 //This is needed to delete the char from original position\cf0 \
-        \}\
-        cout << \cf3 "String is "\cf0  << str << endl;\
-    \}\
-    
-\f0\b \cf2 return
-\f1\b0 \cf0  str;\
-\}\
-\
-\pard\pardeftab720
-
-\f0\b \cf2 int
-\f1\b0 \cf0  
-\f0\b main
-\f1\b0 () \
-\{ \
-    \cf5 myClass\cf0  obj;\
-    \cf5 string\cf0  test1 = \cf3 " \ul Khyati\ulnone  is coding         "\cf0 ;\
-\
-    \cf5 string\cf0  test2 = obj.URLify(test1);\
-    cout << \cf3 "String1 is "\cf0  << test1 << \cf3 " and its URLified version is "\cf0  << test2 << endl;\
-    \
-    
-\f0\b \cf2 return
-\f1\b0 \cf0  0;\
-\}\
+    return 0;
 }
