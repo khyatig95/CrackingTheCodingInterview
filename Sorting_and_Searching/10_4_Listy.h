f16b12083320e817c2e3edeed93a5838e18aaa89
khyatig95 <85378102+khyatig95@users.noreply.github.com>
2022-07-17 10:38:36 -0700

    Update 10_4_Listy.h

--- Sorting_and_Searching/10_4_Listy.h
+++ Sorting_and_Searching/10_4_Listy.h
@@ -1,135 +1,45 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
-{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fnil\fcharset0 Menlo-Bold;}
-{\colortbl;\red255\green255\blue255;\red63\green127\blue95;\red127\green0\blue85;\red0\green80\blue50;
-\red0\green0\blue192;\red42\green0\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720
+//You are given an array-like data structure Listy which lacks a size method.
+//It does, however, have an elementAt ( i) method that returns the element at index i in 0( 1) time.
+//If i is beyond the bounds of the data structure, it returns -1.
+//(For this reason, the data structure only supports positive integers.)
+//Assumption : Listy only contains integers
 
-\f0\fs24 \cf2 //You are given an array-like data structure \ul Listy\ulnone  which lacks a size method.\cf0 \
-\cf2 //It does, however, have an elementAt ( i) method that returns the element at index i in 0( 1) time.\cf0 \
-\cf2 //If i is beyond the bounds of the data structure, it returns -1.\cf0 \
-\cf2 //(For this reason, the data structure only supports positive integers.)\cf0 \
-\cf2 //Assumption : \ul Listy\ulnone  only contains integers\cf0 \
-\
-\pard\pardeftab720
+class Listy {
+	private :
+		int* ListyArr; //Dynamic Array
+		int ListySize;
+	public :
+		Listy() {
+			ListySize = 0;
+		}
+		int elementAt(int index);
+		void assignListy(int *arr, int size);
+		void printArr(int* arr, int size);
+};
 
-\f1\b \cf3 class
-\f0\b0 \cf0  \cf4 Listy\cf0  \{\
-	
-\f1\b \cf3 private
-\f0\b0 \cf0  :\
-		
-\f1\b \cf3 int
-\f0\b0 \cf0 * \cf5 ListyArr\cf0 ; \cf2 //Dynamic Array\cf0 \
-		
-\f1\b \cf3 int
-\f0\b0 \cf0  \cf5 ListySize\cf0 ;\
-	
-\f1\b \cf3 public
-\f0\b0 \cf0  :\
-		
-\f1\b \ul Listy
-\f0\b0 () \{\ulnone \
-\pard\pardeftab720
-\cf0 \ul \ulc0 			\cf5 \ulc5 ListySize\cf0 \ulc0  = 0;\ulnone \
-\ul 		\}\ulnone \
-		
-\f1\b \cf3 int
-\f0\b0 \cf0  
-\f1\b elementAt
-\f0\b0 (
-\f1\b \cf3 int
-\f0\b0 \cf0  index);\
-		
-\f1\b \cf3 void
-\f0\b0 \cf0  
-\f1\b assignListy
-\f0\b0 (
-\f1\b \cf3 int
-\f0\b0 \cf0  *arr, 
-\f1\b \cf3 int
-\f0\b0 \cf0  size);\
-		
-\f1\b \cf3 void
-\f0\b0 \cf0  
-\f1\b printArr
-\f0\b0 (
-\f1\b \cf3 int
-\f0\b0 \cf0 * arr, 
-\f1\b \cf3 int
-\f0\b0 \cf0  size);\
-\};\
-\
-\pard\pardeftab720
+int Listy :: elementAt(int index) {
+	if (index >= ListySize)
+		return -1;
+	return(ListyArr[index]);
+}
 
-\f1\b \cf3 int
-\f0\b0 \cf0  
-\f1\b Listy :: elementAt
-\f0\b0 (
-\f1\b \cf3 int
-\f0\b0 \cf0  index) \{\
-	
-\f1\b \cf3 if
-\f0\b0 \cf0  (index >= \cf5 ListySize\cf0 )\
-		
-\f1\b \cf3 return
-\f0\b0 \cf0  -1;\
-	
-\f1\b \cf3 return
-\f0\b0 \cf0 (\cf5 ListyArr\cf0 [index]);\
-\}\
-\
+void Listy :: assignListy(int *arr, int size) {
+	cout << "Input array is " << endl;
+	printArr(arr, size);
+	ListySize = size;
+	ListyArr = new int(ListySize);
+	for (int i = 0; i < size ; i++) {
+		if (arr[i] < 0)
+			ListyArr[i] = arr[i]*(-1); //Listy supports only +ve vals
+		else
+			ListyArr[i] = arr[i];
+	}
+	printArr(ListyArr, ListySize);
+}
 
-\f1\b \cf3 void
-\f0\b0 \cf0  
-\f1\b Listy :: assignListy
-\f0\b0 (
-\f1\b \cf3 int
-\f0\b0 \cf0  *arr, 
-\f1\b \cf3 int
-\f0\b0 \cf0  size) \{\
-	cout << \cf6 "Input array is "\cf0  << endl;\
-	printArr(arr, size);\
-	\cf5 ListySize\cf0  = size;\
-	\cf5 ListyArr\cf0  = 
-\f1\b \cf3 new
-\f0\b0 \cf0  
-\f1\b \cf3 int
-\f0\b0 \cf0 (\cf5 ListySize\cf0 );\
-	
-\f1\b \cf3 for
-\f0\b0 \cf0  (
-\f1\b \cf3 int
-\f0\b0 \cf0  i = 0; i < size ; i++) \{\
-		
-\f1\b \cf3 if
-\f0\b0 \cf0  (arr[i] < 0)\
-			\cf5 ListyArr\cf0 [i] = arr[i]*(-1); \cf2 //\ul Listy\ulnone  supports only +\ul ve\ulnone  \ul vals\cf0 \ulnone \
-		
-\f1\b \cf3 else
-\f0\b0 \cf0 \
-			\cf5 ListyArr\cf0 [i] = arr[i];\
-	\}\
-	printArr(\cf5 ListyArr\cf0 , \cf5 ListySize\cf0 );\
-\}\
-\
-
-\f1\b \cf3 void
-\f0\b0 \cf0  
-\f1\b Listy :: printArr
-\f0\b0 (
-\f1\b \cf3 int
-\f0\b0 \cf0 * arr, 
-\f1\b \cf3 int
-\f0\b0 \cf0  size) \{\
-	cout << \cf6 "[ "\cf0  ;\
-	
-\f1\b \cf3 for
-\f0\b0 \cf0  (
-\f1\b \cf3 int
-\f0\b0 \cf0  i=0; i<size; i++)\
-		cout << arr[i] << \cf6 " "\cf0 ;\
-	cout << \cf6 "]"\cf0  << endl;\
-\}\
+void Listy :: printArr(int* arr, int size) {
+	cout << "[ " ;
+	for (int i=0; i<size; i++)
+		cout << arr[i] << " ";
+	cout << "]" << endl;
 }
